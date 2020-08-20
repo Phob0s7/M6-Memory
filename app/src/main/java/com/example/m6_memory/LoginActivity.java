082287b7696ee57e151f1db186517cbccea13823
Phob0s7 <villoz_sylvain@hotmail.com>
2020-08-20 23:11:30 +0200

    Tests sur les boutons radio

--- app/src/main/java/com/example/m6_memory/LoginActivity.java
+++ app/src/main/java/com/example/m6_memory/LoginActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.RadioButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private RadioButton radioButtonFemale;
+    private RadioButton radioButtonMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        radioButtonFemale = (RadioButton) findViewById(R.id.Activity_login_femme_radioBtn);
+        radioButtonMan = (RadioButton) findViewById(R.id.Activity_login_homme_radioBtn);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.Activity_login_femme_radioBtn:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.Activity_login_homme_radioBtn:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
